8bba8f5227a361d8729d717c90a96b7d41640489
Maksym Bekuzarov <maksym.bekuzarov@nure.ua>
2018-12-08 20:22:21 +0200

    github pages publish ready

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ProfileUserComponent } from './profile-user/profile-user.component';
 import { ProfileAdminComponent } from './profile-admin/profile-admin.component';
 import { ProfileComponent } from './profile/profile.component';
